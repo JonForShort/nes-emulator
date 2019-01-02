b21a9e24069bfa5ac5428566c613cb678d2fd462
Jon <thejunkjon@gmail.com>
2019-01-02 02:43:49 -0800

    adding support for unit tests

---
+++ source/cpu/test.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required (VERSION 3.7)
+
+# boost
+set (boost-include ${DIR_ROOT_OUT}/boost/include)
+set (boost-lib     ${DIR_ROOT_OUT}/boost/lib)
+
+set (sources cpu_test.cc)
+
+add_executable (cpu_test ${sources})
+
+target_link_libraries (cpu_test cpu)
+target_link_libraries (cpu_test ${boost-lib}/libboost_unit_test_framework.a)
+
+target_include_directories (cpu_test PRIVATE ${boost-include})
+
+add_test(NAME cpu_test COMMAND cpu_test)
