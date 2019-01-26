0ec470673bd9c35d0061261c7e864b4de1f2c0b6
Jon <thejunkjon@gmail.com>
2019-01-26 12:18:15 -0800

    adding more tests for opcodes 0x02 through 0x04

--- source/cpu/cpu_test.cc
+++ source/cpu/cpu_test.cc
@@ -34,11 +34,16 @@ namespace jdi = jones::disassemble;
 
 namespace {
 
-void check_invalid_instruction(const jde::instruction &instruction) {
+void check_instruction_invalid(const jde::instruction &instruction) {
   BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::INVALID);
   BOOST_CHECK(instruction.decoded_opcode.value == 0);
 
   BOOST_CHECK(instruction.decoded_operand.type == operand_type::INVALID);
+  BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0x00));
+}
+
+void check_instruction_no_operand(const jde::instruction &instruction) {
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::NONE);
   BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0x00));
 }
 
@@ -57,42 +62,114 @@ BOOST_AUTO_TEST_CASE(disasemble_test_and_immediate) {
   BOOST_CHECK(first_instruction == "AND #$2C");
 }
 
+//
+// Test: Decodes an instruction that is too small.  In this case, an empty instruction.
+//
 BOOST_AUTO_TEST_CASE(decode_invalid_instruction_too_small) {
 
   uint8_t empty_instruction[] = {};
 
   const auto instruction = jde::decode(empty_instruction, sizeof(empty_instruction));
-  check_invalid_instruction(instruction);
+  check_instruction_invalid(instruction);
 }
 
-BOOST_AUTO_TEST_CASE(decode_brk_instruction_valid) {
+//
+// Test: Decodes opcode value 0x00.
+//
+BOOST_AUTO_TEST_CASE(decode_0x00_brk_instruction_valid) {
 
   uint8_t brk_instruction[] = {0x00};
 
   const auto instruction = jde::decode(brk_instruction, sizeof(brk_instruction));
   BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::BRK);
   BOOST_CHECK(instruction.decoded_opcode.value == 0x00);
-
-  BOOST_CHECK(instruction.decoded_operand.type == operand_type::NONE);
-  BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0x00));
+  check_instruction_no_operand(instruction);
 }
 
-BOOST_AUTO_TEST_CASE(decode_ora_instruction_indexed_indirect_valid) {
+//
+// Test: Decodes opcode value 0x01 with operand value set to 0xFF
+//
+BOOST_AUTO_TEST_CASE(decode_0x01_ora_instruction_valid) {
 
   uint8_t ora_instruction[] = {0x01, 0xFF};
 
   const auto instruction = jde::decode(ora_instruction, sizeof(ora_instruction));
   BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::ORA);
   BOOST_CHECK(instruction.decoded_opcode.value == 0x01);
-
   BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
   BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0xFF));
 }
 
-BOOST_AUTO_TEST_CASE(decode_ora_instruction_indexed_indirect_invalid_too_small) {
+//
+// Test: Decodes opcode value 0x01 with missing operand value.
+//
+BOOST_AUTO_TEST_CASE(decode_0x01_ora_instruction_invalid_too_small) {
 
   uint8_t ora_instruction[] = {0x01};
 
   const auto instruction = jde::decode(ora_instruction, sizeof(ora_instruction));
-  check_invalid_instruction(instruction);
+  check_instruction_invalid(instruction);
+}
+
+//
+// Test: Decodes opcode value 0x02.
+//
+BOOST_AUTO_TEST_CASE(decode_0x02_stp_instruction_valid) {
+
+  uint8_t stp_instruction[] = {0x02};
+
+  const auto instruction = jde::decode(stp_instruction, sizeof(stp_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::STP);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x02);
+  check_instruction_no_operand(instruction);
+}
+
+//
+// Test: Decodes opcode value 0x03 with operand value set to 0xFF.
+//
+BOOST_AUTO_TEST_CASE(decode_0x03_slo_instruction_valid) {
+
+  uint8_t slo_instruction[] = {0x03, 0xFF};
+
+  const auto instruction = jde::decode(slo_instruction, sizeof(slo_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::SLO);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x03);
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
+  BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0xFF));
+}
+
+//
+// Test: Decodes opcode value 0x03 with missing operand value.
+//
+BOOST_AUTO_TEST_CASE(decode_0x03_slo_instruction_invalid_too_small) {
+
+  uint8_t slo_instruction[] = {0x03};
+
+  const auto instruction = jde::decode(slo_instruction, sizeof(slo_instruction));
+  check_instruction_invalid(instruction);
+}
+
+//
+// Test: Decodes opcode value 0x04 with operand value set to 0xFF.
+//
+BOOST_AUTO_TEST_CASE(decode_0x04_nop_instruction_valid) {
+
+  uint8_t nop_instruction[] = {0x04, 0xFF};
+
+  const auto instruction = jde::decode(nop_instruction, sizeof(nop_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::NOP);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x04);
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
+  BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0xFF));
+}
+
+//
+// Test: Decodes opcode value 0x04 with missing operand type.
+//
+BOOST_AUTO_TEST_CASE(decode_0x04_nop_instruction_invalid_too_small) {
+
+  uint8_t nop_instruction[] = {0x04};
+
+  const auto instruction = jde::decode(nop_instruction, sizeof(nop_instruction));
+  check_instruction_invalid(instruction);
 }
