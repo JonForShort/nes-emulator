0ff54e1524349d55730837fe9373c56e175997d6
Jon <thejunkjon@gmail.com>
2019-02-03 16:14:56 -0800

    refactoring unit tests to make it easier to add more

--- source/cpu/cpu_test.cc
+++ source/cpu/cpu_test.cc
@@ -47,22 +47,88 @@ void check_instruction_no_operand(const jde::instruction &instruction) {
   BOOST_CHECK(std::get<uint8_t>(instruction.decoded_operand.value) == static_cast<uint8_t>(0x00));
 }
 
+void check_disassemble(uint8_t *buffer, size_t length_in_bytes, const char *disassembled_string) {
+  const auto instructions = jdi::disassemble(buffer, length_in_bytes);
+  BOOST_CHECK(instructions.instructions.size() == 1);
+
+  const auto &first_instruction = instructions.instructions[0];
+  BOOST_CHECK(first_instruction == disassembled_string);
+}
+
 } // namespace
 
 BOOST_AUTO_TEST_CASE(cpu_test) { BOOST_CHECK(true); }
 
 //
-// Test: Disassembles AND instruction with opcode 0x29.
+// Test: Disassembles opcode 0x00.
 //
-BOOST_AUTO_TEST_CASE(disasemble_0x29_and_instruction_valid) {
+BOOST_AUTO_TEST_CASE(disassemble_0x00_instruction_valid) {
+  uint8_t binary_instruction[] = {0x00};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "BRK");
+}
 
-  uint8_t and_instruction[] = {0x29, 0x2C};
+//
+// Test: Disassembles opcode 0x01.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x01_instruction_valid) {
+  uint8_t binary_instruction[] = {0x01, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "ORA ($FF,X)");
+}
 
-  const auto instructions = jdi::disassemble(and_instruction, sizeof(and_instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
+//
+// Test: Disassembles opcode 0x02.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x02_instruction_valid) {
+  uint8_t binary_instruction[] = {0x02};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "STP");
+}
 
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "AND #$2C");
+//
+// Test: Disassembles opcode 0x03.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x03_slo_instruction_valid) {
+  uint8_t binary_instruction[] = {0x03, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "SLO ($FF,X)");
+}
+
+//
+// Test: Disassembles opcode 0x04.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x04_instruction_valid) {
+  uint8_t binary_instruction[] = {0x04, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "NOP $FF");
+}
+
+//
+// Test: Disassembles opcode 0x05.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x05_instruction_valid) {
+  uint8_t binary_instruction[] = {0x05, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "ORA $FF");
+}
+
+//
+// Test: Disassembles opcode 0x06.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x06_instruction_valid) {
+  uint8_t binary_instruction[] = {0x06, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "ASL $FF");
+}
+
+//
+// Test: Disassembles opcode 0x07.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x07_instruction_valid) {
+  uint8_t binary_instruction[] = {0x07, 0xFF};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "SLO $FF");
+}
+
+//
+// Test: Disassembles opcode 0x29.
+//
+BOOST_AUTO_TEST_CASE(disassemble_0x29_instruction_valid) {
+  uint8_t binary_instruction[] = {0x29, 0x2C};
+  check_disassemble(binary_instruction, sizeof(binary_instruction), "AND #$2C");
 }
 
 //
@@ -89,20 +155,6 @@ BOOST_AUTO_TEST_CASE(decode_0x00_brk_instruction_valid) {
   BOOST_CHECK(instruction.decoded_opcode.value == 0x00);
 
   check_instruction_no_operand(instruction);
-}
-
-//
-// Test: Disassembles opcode value 0x00.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x00_brk_instruction_valid) {
-
-  uint8_t brk_instruction[] = {0x00};
-
-  const auto instructions = jdi::disassemble(brk_instruction, sizeof(brk_instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "BRK");
 }
 
 //
@@ -134,20 +186,6 @@ BOOST_AUTO_TEST_CASE(decode_0x01_ora_instruction_invalid_too_small) {
 }
 
 //
-// Test: Disassembles opcode value 0x01.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x01_ora_instruction_valid) {
-
-  uint8_t ora_instruction[] = {0x01, 0xFF};
-
-  const auto instructions = jdi::disassemble(ora_instruction, sizeof(ora_instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "ORA ($FF,X)");
-}
-
-//
 // Test: Decodes opcode value 0x02.
 //
 BOOST_AUTO_TEST_CASE(decode_0x02_stp_instruction_valid) {
@@ -160,20 +198,6 @@ BOOST_AUTO_TEST_CASE(decode_0x02_stp_instruction_valid) {
   BOOST_CHECK(instruction.decoded_opcode.value == 0x02);
 
   check_instruction_no_operand(instruction);
-}
-
-//
-// Test: Disassembles opcode value 0x02.
-//
-BOOST_AUTO_TEST_CASE(disassemble_0x02_stp_instruction_valid) {
-
-  uint8_t stp_instruction[] = {0x02};
-
-  const auto instructions = jdi::disassemble(stp_instruction, sizeof(stp_instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "STP");
 }
 
 //
@@ -202,20 +226,6 @@ BOOST_AUTO_TEST_CASE(decode_0x03_slo_instruction_invalid_too_small) {
 }
 
 //
-// Test: Disassembles opcode value 0x03.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x03_slo_instruction_valid) {
-
-  uint8_t slo_instruction[] = {0x03, 0xFF};
-
-  const auto instructions = jdi::disassemble(slo_instruction, sizeof(slo_instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "SLO ($FF,X)");
-}
-
-//
 // Test: Decodes opcode value 0x04 with operand value set to 0xFF.
 //
 BOOST_AUTO_TEST_CASE(decode_0x04_nop_instruction_valid) {
@@ -238,20 +248,6 @@ BOOST_AUTO_TEST_CASE(decode_0x04_nop_instruction_invalid_too_small) {
 
   const auto instruction = jde::decode(nop_instruction, sizeof(nop_instruction));
   check_instruction_invalid(instruction);
-}
-
-//
-// Test: Disassembles opcode value 0x04.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x04_nop_instruction_valid) {
-
-  uint8_t instruction[] = {0x04, 0xFF};
-
-  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "NOP $FF");
 }
 
 //
@@ -280,20 +276,6 @@ BOOST_AUTO_TEST_CASE(decode_0x05_ora_instruction_invalid_too_small) {
 }
 
 //
-// Test: Disassembles opcode value 0x05.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x05_ora_instruction_valid) {
-
-  uint8_t instruction[] = {0x05, 0xFF};
-
-  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "ORA $FF");
-}
-
-//
 // Test: Decodes opcode value 0x06 with operand value set to 0xFF
 //
 BOOST_AUTO_TEST_CASE(decode_0x06_asl_instruction_valid) {
@@ -316,20 +298,6 @@ BOOST_AUTO_TEST_CASE(decode_0x06_asl_instruction_invalid_too_small) {
 
   const auto instruction = jde::decode(asl_instruction, sizeof(asl_instruction));
   check_instruction_invalid(instruction);
-}
-
-//
-// Test: Disassembles opcode value 0x06.
-//
-BOOST_AUTO_TEST_CASE(disasemble_0x06_asl_instruction_valid) {
-
-  uint8_t instruction[] = {0x06, 0xFF};
-
-  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
-  BOOST_CHECK(instructions.instructions.size() == 1);
-
-  const auto &first_instruction = instructions.instructions[0];
-  BOOST_CHECK(first_instruction == "ASL $FF");
 }
 
 //
@@ -520,4 +488,91 @@ BOOST_AUTO_TEST_CASE(disasemble_0x0C_nop_instruction_valid) {
 
   const auto &first_instruction = instructions.instructions[0];
   BOOST_CHECK(first_instruction == "NOP $EEFF");
+}
+
+//
+// Test: Decodes opcode value 0x0D.
+//
+BOOST_AUTO_TEST_CASE(decode_0x0D_ora_instruction_valid) {
+
+  uint8_t binary_instruction[] = {0x0D, 0xFF, 0xEE};
+
+  const auto instruction = jde::decode(binary_instruction, sizeof(binary_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::ORA);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x0D);
+
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
+  BOOST_CHECK(std::get<uint16_t>(instruction.decoded_operand.value) == static_cast<uint16_t>(0xEEFF));
+}
+
+//
+// Test: Disassembles opcode value 0x0D.
+//
+BOOST_AUTO_TEST_CASE(disasemble_0x0D_ora_instruction_valid) {
+
+  uint8_t instruction[] = {0x0D, 0xFF, 0xEE};
+
+  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
+  BOOST_CHECK(instructions.instructions.size() == 1);
+
+  const auto &first_instruction = instructions.instructions[0];
+  BOOST_CHECK(first_instruction == "ORA $EEFF");
+}
+
+//
+// Test: Decodes opcode value 0x0E.
+//
+BOOST_AUTO_TEST_CASE(decode_0x0E_asl_instruction_valid) {
+
+  uint8_t binary_instruction[] = {0x0E, 0xFF, 0xEE};
+
+  const auto instruction = jde::decode(binary_instruction, sizeof(binary_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::ASL);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x0E);
+
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
+  BOOST_CHECK(std::get<uint16_t>(instruction.decoded_operand.value) == static_cast<uint16_t>(0xEEFF));
+}
+
+//
+// Test: Disassembles opcode value 0x0E.
+//
+BOOST_AUTO_TEST_CASE(disasemble_0x0E_asl_instruction_valid) {
+
+  uint8_t instruction[] = {0x0E, 0xFF, 0xEE};
+
+  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
+  BOOST_CHECK(instructions.instructions.size() == 1);
+
+  const auto &first_instruction = instructions.instructions[0];
+  BOOST_CHECK(first_instruction == "ASL $EEFF");
+}
+
+//
+// Test: Decodes opcode value 0x0F.
+//
+BOOST_AUTO_TEST_CASE(decode_0x0F_slo_instruction_valid) {
+
+  uint8_t binary_instruction[] = {0x0F, 0xFF, 0xEE};
+
+  const auto instruction = jde::decode(binary_instruction, sizeof(binary_instruction));
+  BOOST_CHECK(instruction.decoded_opcode.type == opcode_type::SLO);
+  BOOST_CHECK(instruction.decoded_opcode.value == 0x0F);
+
+  BOOST_CHECK(instruction.decoded_operand.type == operand_type::MEMORY);
+  BOOST_CHECK(std::get<uint16_t>(instruction.decoded_operand.value) == static_cast<uint16_t>(0xEEFF));
+}
+
+//
+// Test: Disassembles opcode value 0x0F.
+//
+BOOST_AUTO_TEST_CASE(disasemble_0x0F_slo_instruction_valid) {
+
+  uint8_t instruction[] = {0x0F, 0xFF, 0xEE};
+
+  const auto instructions = jdi::disassemble(instruction, sizeof(instruction));
+  BOOST_CHECK(instructions.instructions.size() == 1);
+
+  const auto &first_instruction = instructions.instructions[0];
+  BOOST_CHECK(first_instruction == "SLO $EEFF");
 }
