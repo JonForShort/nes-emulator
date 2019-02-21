80d56f91b95cfd4ea6d7ba4881c4569332255e1b
Jon <thejunkjon@gmail.com>
2019-02-21 10:33:41 -0800

    updating debugger to remove unnecessary arguments

--- source/debugger/debugger.cc
+++ source/debugger/debugger.cc
@@ -1,7 +1,7 @@
 //
 // MIT License
 //
-// Copyright 2018
+// Copyright 2018-2019
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
@@ -38,65 +38,41 @@ namespace ip = boost::interprocess;
 namespace jo = jones;
 
 int main(int argc, char *argv[]) {
-
-  std::string fileArgument;
-  std::string outputArgument;
-  int offsetArgument;
-  int instructionCountArgument;
-  bool disassembleArgument;
-  bool debuggerArgument;
-
+  std::string file_argument;
+  bool has_file_argument;
   try {
     po::options_description desc{"Options"};
     desc.add_options()
       ("help,h", "Help screen")
-      ("file,f", po::value<std::string>(&fileArgument)->required(), "path to binary file")
-      ("output,o", po::value<std::string>(&outputArgument), "directory path to write output")
-      ("offset,s", po::value<int>(&offsetArgument)->default_value(0), "byte offset from start of binary file")
-      ("count,c", po::value<int>(&instructionCountArgument)->default_value(0), "number of instructions to execute")
-      ("disassemble,d", po::bool_switch(&disassembleArgument), "perform disassemble operation")
-      ("debugger,g", po::bool_switch(&debuggerArgument), "performs operation in a debugger");
+      ("file,f", po::value<std::string>(&file_argument), "path to file");
 
     po::variables_map vm;
-    po::store(parse_command_line(argc, argv, desc), vm);
+    const auto command_line_args = parse_command_line(argc, argv, desc);
+    po::store(command_line_args, vm);
     po::notify(vm);
+
+    has_file_argument = vm.count("file");
   } catch (const po::error &ex) {
     std::cerr << ex.what() << '\n';
     return -1;
   }
 
-  const fs::path pathToBinaryFile(fileArgument);
-  if (!fs::exists(pathToBinaryFile)) {
-    std::cout << "binary file path does not exist; please check path"
-              << std::endl;
-    return -2;
+  const fs::path file_path(file_argument);
+  if (has_file_argument) {
+    if (!fs::exists(file_path)) {
+      std::cout << "file does not exist; please check path" << std::endl;
+      return -2;
+    }
+    if (fs::is_directory(file_path)) {
+      std::cout << "file is a directory; please check path" << std::endl;
+      return -3;
+    }
   }
 
-  if (fs::is_directory(pathToBinaryFile)) {
-    std::cout << "binary file path is a directory; please check path"
-              << std::endl;
-    return -3;
-  }
-
-  const auto performDisassemble = disassembleArgument;
-  if (performDisassemble) {
-    std::cout << "performing disassemble operation" << std::endl;
-    const ip::file_mapping mappedBinary(pathToBinaryFile.c_str(),
-                                        ip::mode_t::read_only);
-    const ip::mapped_region mappedRegion(mappedBinary, ip::mode_t::read_only);
-    const auto &mappedStartAddress = mappedRegion.get_address();
-    const auto &mappedSize = mappedRegion.get_size();
-    jo::cpu cpu(mappedStartAddress);
-    cpu.run();
-  }
-
-  const auto performDebugger = debuggerArgument;
-  if (performDebugger) {
-    jo::main_screen screen;
-    screen.initialize();
-    screen.show();
-    screen.release();
-  }
+  jo::main_screen screen;
+  screen.initialize();
+  screen.show();
+  screen.release();
 
   return 0;
 }
