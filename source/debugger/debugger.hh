2147afb60662723f475874cc389e733e0b2f2bc4
Jon <thejunkjon@gmail.com>
2019-03-30 01:12:46 -0700

    initial attempt at debugger interface

--- source/debugger/debugger.hh
+++ source/debugger/debugger.hh
@@ -21,4 +21,66 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-#include "debugger.hh"
+#ifndef JONES_DEBUGGER_DEBUGGER_HH
+#define JONES_DEBUGGER_DEBUGGER_HH
+
+#include <cstdint>
+
+namespace jones::debugger {
+
+using r8_t = uint8_t;
+using r16_t = uint16_t;
+
+enum class register_t {
+
+  // program counter (16 bit)
+  PC,
+
+  // accumulator (8 bit)
+  AC,
+
+  // X register (8 bit)
+  X,
+
+  // Y register (8 bit)
+  Y,
+
+  // status register (8 bit)
+  SR,
+
+  // stack pointer (8 bit)
+  SP
+};
+
+template <register_t R>
+struct register_traits {
+
+  typedef r8_t type;
+
+  static constexpr uint8_t length = sizeof(type);
+};
+
+template <>
+struct register_traits<register_t::PC> {
+
+  typedef r16_t type;
+
+  static constexpr uint8_t length = sizeof(type);
+};
+
+template <register_t R>
+using register_type = typename register_traits<R>::type;
+
+template <register_t R>
+void write_register(register_t reg, register_type<R> value);
+
+template <register_t R>
+register_type<R> read_register(register_t reg);
+
+void write_memory(uint16_t address, uint16_t value);
+
+void read_memory(uint16_t address);
+
+} // namespace jones::debugger
+
+#endif // JONES_DEBUGGER_DEBUGGER_HH
