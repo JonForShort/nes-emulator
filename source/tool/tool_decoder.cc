2bb446200ec86d3a2706d317ccd23ab5cdd9db7b
Jon <thejunkjon@gmail.com>
2019-04-01 18:18:38 -0700

    adding test code that writes a png file.  This will be changed in the future to write the tiles to a png file

--- source/tool/tool_decoder.cc
+++ source/tool/tool_decoder.cc
@@ -22,8 +22,20 @@
 // SOFTWARE.
 //
 #include <boost/core/ignore_unused.hpp>
+#include <boost/filesystem.hpp>
+#include <boost/gil.hpp>
+
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wignored-qualifiers"
+
+#include <boost/gil/extension/io/png.hpp>
+
+#pragma GCC diagnostic pop
 
 #include "tool_decoder.hh"
+
+namespace fs = boost::filesystem;
+namespace bg = boost::gil;
 
 namespace jones::tool {
 
@@ -31,6 +43,23 @@ void decode_chrrom(uint8_t *base_address, size_t length_in_bytes, const char *ou
   boost::ignore_unused(base_address);
   boost::ignore_unused(length_in_bytes);
   boost::ignore_unused(out_path);
+
+  const fs::path image_path{out_path};
+  const fs::path file_path = image_path / "redsquare.png";
+
+  bg::rgb8_image_t img(512, 512);
+  auto view = bg::view(img);
+  view(1, 1) = bg::rgb8_pixel_t(255, 0, 0);
+  bg::write_view(file_path.string(), view, bg::image_write_info<bg::png_tag>());
+
+  //  for (size_t i = 0; i < length_in_bytes; i++) {
+  //    boost::gil::rgb8_image_t image(8, 8);
+  //    boost::gil::rgb8_pixel_t pixel(0, 0, 10);
+  //    boost::gil::rgb8_view_t view;
+  //    image.set
+  //    uint8_t tile_row_first_layer = base_address[i++];
+  //    uint8_t tile_row_second_layer = base_address[i++];
+  //  }
 }
 
 } // namespace jones::tool
