82e40041f7abb9ce4a4257a27f05b607ea7bad1d
Jon <thejunkjon@gmail.com>
2019-05-08 00:40:14 -0700

    adding code coverage script

--- CodeCoverage.cmake
+++ CodeCoverage.cmake
@@ -1,4 +1,4 @@
 if ("$ENV{CODE_COVERAGE}" STREQUAL "1")
-  set(JONES_CXX_FLAGS_DEBUG "${JONES_CXX_FLAGS_DEBUG} --coverage")
+  set(JONES_CXX_FLAGS_DEBUG "${JONES_CXX_FLAGS_DEBUG} -fprofile-arcs -ftest-coverage --coverage")
 endif()
 
--- scripts/env.sh
+++ scripts/env.sh
@@ -1,11 +1,15 @@
 #!/usr/bin/env bash
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-OUT_DIR="${SCRIPT_DIR}/../out/jones"
+
+JONES_OUT_DIR=${SCRIPT_DIR}/../out/jones
+
+JONES_BUILD_DIR=${SCRIPT_DIR}/../build
 
 jones_tool() {
 
-    JONES_TOOL=${OUT_DIR}/bin/jones-tool
+    JONES_TOOL=${JONES_OUT_DIR}/bin/jones-tool
+
     if [ ! -f "${JONES_TOOL}" ]; then
 	echo "error: unable to find tool; path is not valid [${JONES_TOOL}]"
 	return -1
@@ -16,12 +20,24 @@ jones_tool() {
 
 jones_build() {
 
-    JONES_BUILD_DIR=${SCRIPT_DIR}/../build
-
     mkdir -p ${JONES_BUILD_DIR}
 
     pushd ${JONES_BUILD_DIR}
         cmake -DCMAKE_BUILD_TYPE=Debug ..
-        make "$@" && make test
+        make "$@"
     popd
+}
+
+jones_test() {
+
+    jones_build "$@"
+
+    pushd ${JONES_BUILD_DIR}
+        make test
+    popd
+}
+
+jones_test_code_coverage() {
+
+    CODE_COVERAGE=1 jones_test
 }
