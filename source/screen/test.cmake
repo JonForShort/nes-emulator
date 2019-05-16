a8394793d5c6c119009e44cc89eeca11ca0035f9
Jon <thejunkjon@gmail.com>
2019-05-16 01:10:09 -0700

    adding screen module to represent a screen concept

---
+++ source/screen/test.cmake
@@ -0,0 +1,16 @@
+cmake_minimum_required (VERSION 3.7)
+
+# boost
+set (boost-include ${DIR_ROOT_OUT}/boost/include)
+set (boost-lib     ${DIR_ROOT_OUT}/boost/lib)
+
+set (sources screen_test.cc)
+
+add_executable (screen_test ${sources})
+
+target_link_libraries (screen_test screen)
+target_link_libraries (screen_test ${boost-lib}/libboost_unit_test_framework.a)
+
+target_include_directories (screen_test PRIVATE ${boost-include})
+
+add_test(NAME screen_test COMMAND screen_test)
