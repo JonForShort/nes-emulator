a4ed1ec322a310a5cca75c0c42054b6c7c690071
Jon <thejunkjon@gmail.com>
2019-05-25 19:38:52 -0700

    moving memory mapped behavior for controller to mapped controller

--- source/jones/controller/include/mapped_controller.hh
+++ source/jones/controller/include/mapped_controller.hh
@@ -21,32 +21,65 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 //
-#ifndef JONES_NES_NES_CONTROLLER_HH
-#define JONES_NES_NES_CONTROLLER_HH
+#ifndef JONES_CONTROLLER_CONTROLLER_HH
+#define JONES_CONTROLLER_CONTROLLER_HH
 
-#include "controller.hh"
 #include "memory.hh"
 
-#include <boost/core/ignore_unused.hpp>
+namespace jones::controller {
 
-namespace jones {
+enum class button {
+  BUTTON_A,
 
-class mapped_nes_controller {
-public:
-  explicit mapped_nes_controller(controller::controller *controller) : controller_(controller) {}
+  BUTTON_B,
 
-  auto read(const uint16_t address) const -> uint8_t {
-    return controller_->read(address);
-  }
+  BUTTON_SELECT,
 
-  auto write(const uint16_t address, const uint8_t data) -> void {
-    controller_->write(address, data);
-  }
+  BUTTON_START,
 
-private:
-  controller::controller *const controller_;
+  BUTTON_UP,
+
+  BUTTON_DOWN,
+
+  BUTTON_LEFT,
+
+  BUTTON_RIGHT
 };
 
-} // namespace jones
+enum class button_state {
+  BUTTON_STATE_DOWN,
 
-#endif // JONES_NES_NES_CONTROLLER_HH
+  BUTTON_STATE_UP,
+};
+
+enum class controller_state {
+  CONNECTED,
+
+  DISCONNECTED
+};
+
+class controller final {
+public:
+  explicit controller(const memory &memory);
+
+  ~controller();
+
+  auto set_button_state(button_state state) -> void;
+
+  auto set_controller_state(controller_state state) -> void;
+
+  auto read(uint16_t address) -> uint8_t;
+
+  auto write(uint16_t address, uint8_t data) -> void;
+
+private:
+  class impl;
+
+  std::unique_ptr<impl> impl_;
+};
+
+using controller_ptr = controller *;
+
+} // namespace jones::controller
+
+#endif // JONES_CONTROLLER_CONTROLLER_HH
