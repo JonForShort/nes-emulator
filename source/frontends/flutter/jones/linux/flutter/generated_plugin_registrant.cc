6e86245bb690087f9ff96b86936b8f0921f87bad
thejunkjon <thejunkjon@gmail.com>
2021-02-14 00:25:31 -0800

    Updating jones project to use latest flutter.

--- source/frontends/flutter/jones/linux/flutter/generated_plugin_registrant.cc
+++ source/frontends/flutter/jones/linux/flutter/generated_plugin_registrant.cc
@@ -4,6 +4,10 @@
 
 #include "generated_plugin_registrant.h"
 
+#include <native_jones/native_jones_plugin.h>
 
 void fl_register_plugins(FlPluginRegistry* registry) {
+  g_autoptr(FlPluginRegistrar) native_jones_registrar =
+      fl_plugin_registry_get_registrar_for_plugin(registry, "NativeJonesPlugin");
+  native_jones_plugin_register_with_registrar(native_jones_registrar);
 }
--- source/frontends/flutter/jones/linux/flutter/generated_plugins.cmake
+++ source/frontends/flutter/jones/linux/flutter/generated_plugins.cmake
@@ -3,6 +3,7 @@
 #
 
 list(APPEND FLUTTER_PLUGIN_LIST
+  native_jones
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
